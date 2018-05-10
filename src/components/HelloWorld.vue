f9c348277ea186154061e5e41695bfc5099010fa
Jordan Fokoua <jfokoua@gmail.com>
2018-05-10 12:42:52 +0100

    :sparkles: Create Product card component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,43 @@
 <template>
-  <div >
+  <div>
     <el-container>
+      <!-- Page Header -->
       <el-header>
         <Navigation></Navigation>
       </el-header>
-      <el-container>
+
+      <!-- Page Body -->
         <el-container>
           <el-main>
-              <SideNav></SideNav>
-            <Carousel></Carousel>
+            <!-- Top page -->
+            <el-row>
+              <!-- Side menu navigation -->
+              <el-col :span="8">
+                <SideNav></SideNav>
+              </el-col>
+
+              <!-- Carousel -->
+              <el-col :span="16">
+                  <Carousel></Carousel>
+              </el-col>
+            </el-row>
+
+            <!-- Mid Page -->
+            <el-row>
+              <!-- Email subscription -->
+              <el-col :span="8">
+              </el-col>
+
+              <!-- Product cards + Carousel -->
+              <el-col :span="16">
+                <ProdCard></ProdCard>
+              </el-col>
+              
+            </el-row>
           </el-main>
           <el-footer>Footer</el-footer>
         </el-container>
       </el-container>
-    </el-container>
 
   </div>
 </template>
@@ -22,12 +46,14 @@
   import Navigation from "./Navigation/Navigation";
   import Carousel from "./Carousel/Carousel";
   import SideNav from "./SideNav/SideNav";
+  import ProdCard from "./Product-Card/Product-Card";
   export default {
     name: "HelloWorld",
     components: {
       Navigation,
       Carousel,
-      SideNav
+      SideNav,
+      ProdCard
     },
     data() {
       return {
---
+++ src/components/Product-Card/Product-Card.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-row>
+  <el-col :span="8" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0">
+    <el-card :body-style="{ padding: '0px' }">
+      <img src="" class="image">
+      <div style="padding: 14px;">
+        <span>Yummy hamburger</span>
+        <div class="bottom clearfix">
+          <time class="time">{{ currentDate }}</time>
+          <el-button type="text" class="button">Operating button</el-button>
+        </div>
+      </div>
+    </el-card>
+  </el-col>
+</el-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentDate: new Date()
+    };
+  }
+};
+</script>
+
+
+<style>
+@import "./Product-Card.scss";
+</style>
