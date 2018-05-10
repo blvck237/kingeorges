f0f4e7f8f81bb6b9d97a276b38590cb4138df028
Jordan Fokoua <jfokoua@gmail.com>
2018-05-10 23:00:26 +0100

    :lipstick: Update Side nav UI

--- src/components/SideNav/SideNav.vue
+++ src/components/SideNav/SideNav.vue
@@ -1,35 +1,28 @@
 <template>
   <el-row class="tac">
     <el-col :span="12">
-      <h5>Nos Services</h5>
-      <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
-     
+      <!-- Navigation title -->
+      <el-menu  class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
+        <div class="nav-title">Nos Services</div>
         <el-menu-item index="1">
-          <i class="el-icon-menu"></i>
           <span>Broderie</span>
         </el-menu-item>
         <el-menu-item index="2">
-          <i class="el-icon-menu"></i>
           <span>Conception</span>
         </el-menu-item>
         <el-menu-item index="3" >
-          <i class="el-icon-document"></i>
           <span>Creation Graphique</span>
         </el-menu-item>
         <el-menu-item index="4">
-          <i class="el-icon-setting"></i>
           <span>Gravure</span>
         </el-menu-item>
         <el-menu-item index="5">
-          <i class="el-icon-setting"></i>
           <span>Impression Numerique</span>
         </el-menu-item>
-        <el-menu-item index="4">
-          <i class="el-icon-setting"></i>
+        <el-menu-item index="6">
           <span>Sérigraphie</span>
         </el-menu-item>
-        <el-menu-item index="4">
-          <i class="el-icon-setting"></i>
+        <el-menu-item index="7">
           <span>Conseil</span>
         </el-menu-item>
       </el-menu>
@@ -41,15 +34,18 @@
 
 
 <script>
-  export default {
-    methods: {
-      handleOpen(key, keyPath) {
-        console.log(key, keyPath);
-      },
-      handleClose(key, keyPath) {
-        console.log(key, keyPath);
-      }
+export default {
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
     }
-  };
-
+  }
+};
 </script>
+
+<style>
+@import './SideNav.scss';
+</style>
