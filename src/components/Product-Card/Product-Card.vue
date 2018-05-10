98ad4ee781412d4cbf4d7d28cc353d5c9716808a
Jordan Fokoua <jfokoua@gmail.com>
2018-05-10 15:11:35 +0100

    :lipstick: Update product card UI

--- src/components/Product-Card/Product-Card.vue
+++ src/components/Product-Card/Product-Card.vue
@@ -1,31 +1,60 @@
 <template>
   <el-row>
-  <el-col :span="8" v-for="(o, index) in 2" :key="o" :offset="index > 0 ? 2 : 0">
-    <el-card :body-style="{ padding: '0px' }">
-      <img src="" class="image">
-      <div style="padding: 14px;">
-        <span>Yummy hamburger</span>
-        <div class="bottom clearfix">
-          <time class="time">{{ currentDate }}</time>
-          <el-button type="text" class="button">Operating button</el-button>
+    <el-col class="prodcard" :span="8" v-for="(o, index) in 1" :key="o">
+
+      <!-- Card -->
+      <el-card :body-style="{ padding: '0px' }">
+        <img src="../../assets/2.2.jpg" class="image">
+        <div>
+            <!-- Product name -->
+          <span class="prod-name">Sac serigraphie</span>
+          <!-- Product Rating -->
+          <div class="prod-rating">
+            <i class="el-icon-star-on"></i>
+            <i class="el-icon-star-on"></i>
+            <i class="el-icon-star-on"></i>
+            <i class="el-icon-star-on"></i>
+            <i class="el-icon-star-off"></i>
+          </div>
+          <div class="bottom clearfix">
+
+            <!-- Star button -->
+            <el-col :span="5">
+              <el-button type="text" class="button" icon="el-icon-star-on"></el-button>
+            </el-col>
+
+            <!-- Add to cart button -->
+            <el-col :span="14">
+              <el-button type="text" class="button mid-btn">
+                <i class="el-icon-goods el-icon-right"></i>Ajouter au panier</el-button>
+            </el-col>
+
+            <!-- Add btn -->
+            <el-col :span="5">
+              <el-button type="text" class="button" icon="el-icon-plus"></el-button>
+            </el-col>
+
+          </div>
         </div>
-      </div>
-    </el-card>
-  </el-col>
-</el-row>
+      </el-card>
+
+    </el-col>
+  </el-row>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      currentDate: new Date()
-    };
-  }
-};
+  export default {
+    data() {
+      return {
+        currentDate: new Date()
+      };
+    }
+  };
+
 </script>
 
 
 <style>
-@import "./Product-Card.scss";
+  @import "./Product-Card.scss";
+
 </style>
