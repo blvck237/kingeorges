76119a70676e434a95aa5659e6ed6f2c0d0d5382
Jordan Fokoua <jfokoua@gmail.com>
2018-05-10 23:00:35 +0100

    :lipstick: Update Card UI

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -1,11 +1,14 @@
 <template>
   <el-card class="box-card">
-  <div v-for="o in 4" :key="o" class="text item">
-    {{'List item ' + o }}
-  </div>
-</el-card>
+    <h5 class="card-title">Recevez des infos</h5>
+    <p>Recevez par email des informations concernants nos soldes, stocks, etc..</p>
+    <el-input placeholder="Email" v-model="input10" clearable>
+    </el-input>
+    <el-button>Inscription</el-button>
+  </el-card>
 </template>
 
 <style>
-@import './Card.scss';
+  @import './Card.scss';
+
 </style>
