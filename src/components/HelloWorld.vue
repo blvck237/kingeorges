5506b634fbba813ca8b1cc2751b494aa9a53494b
Jordan Fokoua <jfokoua@gmail.com>
2018-05-10 11:31:16 +0100

    :sparkles: Add layout

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,27 +1,40 @@
 <template>
   <div class="hello">
-    <Navigation>
-    </Navigation>
+    <el-container>
+      <el-header>
+        <Navigation></Navigation>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-container>
+          <el-main>
+          </el-main>
+          <el-footer>Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
 
   </div>
 </template>
 
 <script>
-import Navigation from './Navigation/Navigation'
-export default {
-  name: 'HelloWorld',
-  components: {
-    Navigation,
-  },
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import Navigation from "./Navigation/Navigation";
+  export default {
+    name: "HelloWorld",
+    components: {
+      Navigation
+    },
+    data() {
+      return {
+        msg: "Welcome to Your Vue.js App"
+      };
     }
-  }
-}
+  };
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 
 </style>
