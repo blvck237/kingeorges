89db191bd0f5dcd1e534f43aac4964895bba39d2
Jordan Fokoua <jfokoua@gmail.com>
2018-05-10 11:52:54 +0100

    :art: Update layout

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="hello">
+  <div >
     <el-container>
       <el-header>
         <Navigation></Navigation>
       </el-header>
       <el-container>
-        <el-aside width="200px">Aside</el-aside>
         <el-container>
           <el-main>
+            Main
           </el-main>
           <el-footer>Footer</el-footer>
         </el-container>
