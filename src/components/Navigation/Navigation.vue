ce4af61e75a59eaabb37377cae5cde8ba5220fc8
Jordan Fokoua <jfokoua@gmail.com>
2018-05-10 11:30:43 +0100

    :art: Update menu and css

--- src/components/Navigation/Navigation.vue
+++ src/components/Navigation/Navigation.vue
@@ -1,22 +1,29 @@
 <template>
   <div>
-    <el-menu :default-active="1" :active-text-color="'#FFA200'" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-      <el-menu-item index="1">Accueil</el-menu-item>
-      <el-submenu index="2">
+    <el-menu :default-active="8" :active-text-color="'#FFA200'" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+      <img class="logo" src="../../assets/logo.png" alt="Logo Kingeorges">
+      <el-menu-item index="1"><i class="el-icon-goods"></i></el-menu-item>
+      <el-menu-item index="2"><i class="el-icon-search"></i></el-menu-item>
+      <el-menu-item index="3">Contact</el-menu-item>
+      <el-menu-item index="4">L'équipe</el-menu-item>
+      <el-menu-item index="5">Références</el-menu-item>
+      <el-menu-item index="6">Qui-sommes-nous?</el-menu-item>
+      <el-submenu index="7">
         <template slot="title">Produits</template>
-        <el-menu-item index="2-1">Imprimerie</el-menu-item>
-        <el-menu-item index="2-2">Impression numerique</el-menu-item>
-        <el-menu-item index="2-3">Serigraphie moderne</el-menu-item>
-        <el-menu-item index="2-4">Broderie</el-menu-item>
-        <el-menu-item index="2-5">Peinture</el-menu-item>
+        <el-menu-item index="7-1">Imprimerie</el-menu-item>
+        <el-menu-item index="7-2">Impression numerique</el-menu-item>
+        <el-menu-item index="7-3">Serigraphie moderne</el-menu-item>
+        <el-menu-item index="7-4">Broderie</el-menu-item>
+        <el-menu-item index="7-5">Peinture</el-menu-item>
       </el-submenu>
-      <el-menu-item index="3" >Qui-sommes-nous?</el-menu-item>
-      <el-menu-item index="4" >Références</el-menu-item>
-      <el-menu-item index="5" >L'équipe</el-menu-item>
-      <el-menu-item index="5" >Contact</el-menu-item>
+      <el-menu-item index="8">Accueil</el-menu-item>
+
 
     </el-menu>
     <div class="line"></div>
-
   </div>
 </template>
+
+<style>
+@import './Navigation.scss';
+</style>
