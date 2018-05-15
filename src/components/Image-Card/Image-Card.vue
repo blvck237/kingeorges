bcdfba262ddce63b9dac62d7f4926f0cab7af9de
Jordan Fokoua <jfokoua@gmail.com>
2018-05-15 09:26:39 +0100

    :lipstick: Update UI

--- src/components/Image-Card/Image-Card.vue
+++ src/components/Image-Card/Image-Card.vue
@@ -1,9 +1,17 @@
 <template>
-  <el-card class="box-card">
+  <el-card class="box-card" >
+    <div>
       <img src="../../assets/1.jpg" alt="">
+    </div>
 </el-card>
 </template>
 
 <style scoped>
 @import './Image-Card.scss';
+
+
+.el-card-body {
+    padding: 10px !important;
+}
+
 </style>
