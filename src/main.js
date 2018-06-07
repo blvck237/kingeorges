1d0db98b0f0fc43c3b659b837f80bc0bccd6bfbd
Jordan Fokoua <jfokoua@gmail.com>
2018-06-07 16:36:06 +0100

    :heavy_plus_sign: Firebase :heavy_plus_sign: VueFire :sparkles: Initialize firebase

--- src/main.js
+++ src/main.js
@@ -4,17 +4,36 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import VueFire from 'vuefire'
+import firebase from 'firebase'
+import 'firebase/firestore'
+
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 import locale from 'element-ui/lib/locale/lang/en'
 
-Vue.use(ElementUI, { locale })
+Vue.use(VueFire)
+firebase.initializeApp({
+  apiKey: "AIzaSyAotALjGQaaMufKG98VFUfyk7RUXGNWyds",
+  authDomain: "king-geor.firebaseapp.com",
+  databaseURL: "https://king-geor.firebaseio.com",
+  projectId: "king-geor",
+  storageBucket: "king-geor.appspot.com",
+  messagingSenderId: "479648737810"
+})
+export const db = firebase.firestore()
+
+Vue.use(ElementUI, {
+  locale
+})
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
