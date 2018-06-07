5fb9b96e6b0b86afbde33ac0fe18f81274044a27
Jordan Fokoua <jfokoua@gmail.com>
2018-06-07 16:25:06 +0100

    :sparkles: create Product page

---
+++ src/containers/Products.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div class="row">
+    <product-card class="col-4"></product-card>    
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import ProductCard from "../components/Product-Card/Product-Card";
+export default {
+  name: "Products",
+  components: {
+    ProductCard
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style>
+</style>
