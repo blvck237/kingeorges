a1f1324fcfadc921394cd986d11736b216635c81
Jordan Fokoua <jfokoua@gmail.com>
2018-06-22 00:39:58 +0100

    :sparkles: Add contact page to routing

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/containers/Home'
 import Products from '@/containers/Products'
+import Contact from '@/containers/Contact'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/products',
       name: 'Products',
       component: Products
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
     },
   ]
 })
