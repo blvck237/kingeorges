79de37d3e52d9b53cf1c6d104a50d039a317f31f
Jordan Fokoua <jfokoua@gmail.com>
2018-06-22 10:59:44 +0100

    :sparkles: Create team vue

---
+++ src/containers/Team.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="container">
+
+    <div class="header">
+      <h3>Meet the team</h3>
+      <p>Lorem ipsum</p>
+    </div>
+
+    <div class="row">
+
+      <div class="team-member mx-auto d-block">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkPR_gjFUnPgLosi0jRF-uqt46W5VuUGc6qgdT4kd2Irfx4f_b" class="avatar"
+          alt="...">
+        <div class="description">
+          <div class="name">Virginie FOKOUA</div>
+          <div class="role">PDG</div>
+        </div>
+      </div>
+
+      <div class="team-member mx-auto d-block">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkPR_gjFUnPgLosi0jRF-uqt46W5VuUGc6qgdT4kd2Irfx4f_b" class="avatar"
+          alt="...">
+        <div class="description">
+          <div class="name">Nom</div>
+          <div class="role">Role</div>
+        </div>
+      </div>
+
+      <div class="team-member mx-auto d-block">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkPR_gjFUnPgLosi0jRF-uqt46W5VuUGc6qgdT4kd2Irfx4f_b" class="avatar"
+          alt="...">
+        <div class="description">
+          <div class="name">Nom</div>
+          <div class="role">Role</div>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="row">
+
+      <div class="team-member mx-auto d-block">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkPR_gjFUnPgLosi0jRF-uqt46W5VuUGc6qgdT4kd2Irfx4f_b" class="avatar"
+          alt="...">
+        <div class="description">
+          <div class="name">Nom</div>
+          <div class="role">Role</div>
+        </div>
+      </div>
+
+      <div class="team-member mx-auto d-block">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkPR_gjFUnPgLosi0jRF-uqt46W5VuUGc6qgdT4kd2Irfx4f_b" class="avatar"
+          alt="...">
+        <div class="description">
+          <div class="name">Nom</div>
+          <div class="role">Role</div>
+        </div>
+      </div>
+
+      <div class="team-member mx-auto d-block">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkPR_gjFUnPgLosi0jRF-uqt46W5VuUGc6qgdT4kd2Irfx4f_b" class="avatar"
+          alt="...">
+        <div class="description">
+          <div class="name">Nom</div>
+          <div class="role">Role</div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {};
+
+</script>
+
+<style scoped>
+  .avatar {
+    border-radius: 50% !important;
+  }
+
+  .description {
+    margin-top: 0.5em;
+  }
+
+  .name {
+    font-weight: 600;
+    font-size: 16px;
+  }
+
+  .role {
+    font-weight: 200;
+    font-size: 16px;
+  }
+
+  .container {
+    background: #fff;
+    padding: 1em;
+
+  }
+
+  .team-member {
+    font-family: 'Open Sans', sans-serif;
+    padding: 0.5em;
+  }
+
+.header{
+    font-family: 'Open Sans', sans-serif;
+    text-align: left;
+}
+
+  h3 {
+    font-family: 'Oswald', sans-serif !important;
+  }
+
+</style>
