fac210a07e17aca99d60b5d3cac2db39d602e1ad
Jordan Fokoua <jfokoua@gmail.com>
2018-08-01 12:45:54 +0100

    :bug: Clear input model

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -2,7 +2,7 @@
   <el-card class="box-card">
     <h5 class="card-title">Recevez des infos</h5>
     <p>Recevez par email des informations concernants nos soldes, stocks, etc..</p>
-    <el-input placeholder="Email" v-model="input10" clearable>
+    <el-input placeholder="Email" clearable>
     </el-input>
     <el-button>Inscription</el-button>
   </el-card>
