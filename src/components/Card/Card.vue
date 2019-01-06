14318b9fee180f8d3f7fa5d8e8e9b8ae93b9e14e
Jordan Fokoua <jfokoua@gmail.com>
2019-01-06 16:36:25 +0100

    :sparkles: Newsletter subscribtion

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -2,13 +2,29 @@
   <el-card class="box-card">
     <h5 class="card-title">Recevez des infos</h5>
     <p>Recevez par email des informations concernants nos soldes, stocks, etc..</p>
-    <el-input placeholder="Email" clearable>
-    </el-input>
-    <el-button>Inscription</el-button>
+    <el-input v-model="email" placeholder="Email" clearable></el-input>
+    <el-button @click="subscribeNewsletter()">Inscription</el-button>
   </el-card>
 </template>
 
 <style scoped>
-  @import './Card.scss';
-
+@import "./Card.scss";
 </style>
+
+<script>
+export default {
+  props: ["myProp"],
+  data() {
+    return {
+      email: "",
+      msg: ""
+    };
+  },
+  methods: {
+    subscribeNewsletter() {
+      this.$parent.addToNewsletter(this.email);
+      console.log("User mail", this.email);
+    }
+  }
+};
+</script>
